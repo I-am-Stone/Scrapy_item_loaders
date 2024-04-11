9b5bc4ba01edd09fa337e924631c7c147682100d
I-am-Stone <dilipchaulagain86@gmail.com>
2024-04-11 15:52:00 +0545

    item_loader exm

--- item_loders/pipelines.py
+++ item_loders/pipelines.py
@@ -3,6 +3,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pandas as pd
 
 # useful for handling different item types with a single interface
 class ItemLodersPipeline:
